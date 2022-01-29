19c0510e421d989592291654df7cf956f710cc10
bdiplus ravi <bdiplus@bdiplus.com>
2022-01-29 12:55:03 -0500

    v2.0

---
+++ afive.rb
@@ -0,0 +1,36 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class afive < Formula
+  desc "four"
+  homepage "bdiplus.com"
+  url "https://github.com/raviarasan/homebrew-afour/archive/refs/tags/v2.0.tar.gz"
+  sha256 "0d7d22e2922fd13659ef35015ddf5e9df3d89541e72ceeb3beacd470dcd623f1"
+  version "2.0"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin install 'r1.py'
+    bin install 'r.sh'
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test homebrew-afour`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "sh r.sh"
+  end
+end
